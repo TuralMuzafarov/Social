c016adc09623e5ca7ae15bc37a590f463c944d2d
TuralMuzafarov <turalmuzefferli4@gmail.com>
2025-02-02 04:44:55 -0800

    Posts and Users Stores are added

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -1,13 +1,23 @@
 package store
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
 type Store struct {
 	Posts interface {
-		Create(ctx context.Context, post Post) error
+		Create(ctx context.Context, post *Post) error
 	}
 
 	Users interface {
-		Create(ctx context.Context, users User) error
+		Create(ctx context.Context, users *User) error
+	}
+}
+
+func NewStore(db *sql.DB) Store {
+	return Store{
+		Posts: &PostsStore{db: db},
+		Users: &UserStore{db: db},
 	}
 }
--- internal/store/user.go
+++ internal/store/user.go
@@ -1,4 +1,40 @@
 package store
 
+import (
+	"context"
+	"database/sql"
+)
+
 type User struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	CreatedAt string `json:"created_at"`
+}
+
+type UserStore struct {
+	db *sql.DB
+}
+
+func (s *UserStore) Create(ctx context.Context, users *User) error {
+	query := `
+		INSERT INTO posts (username, email, password)
+		VALUES ($1, $2, $3) RETURNING id, created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		users.Username,
+		users.Email,
+		users.Password,
+	).Scan(
+		&users.ID,
+		&users.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
