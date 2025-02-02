b12ae8950b607b173529888ae70496f1162acfb5
TuralMuzafarov <turalmuzefferli4@gmail.com>
2025-02-02 03:07:22 -0800

    Setting The Environment Done

---
+++ cmd/api/api.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type Config struct {
+	addr string
+}
+
+type Application struct {
+	config Config
+}
+
+func (app *Application) mount() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	// posts
+
+	// users
+
+	// auth
+	return r
+}
+
+func (app *Application) run(mux http.Handler) error {
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	env "github.com/TuralMuzafarov/social/internal/env"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Printf("Could not load the environment variables... \n")
+		time.Sleep(time.Second * 3)
+		log.Println("System is starting with default configurations")
+	}
+	cfg := Config{
+		addr: env.GetString("ADDR", ":8081"),
+	}
+	app := &Application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+	log.Fatal(app.run(mux))
+}
